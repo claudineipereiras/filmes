8c33ecd7c081a1ffb7b94c3599bccf5a15bf9291
Claudinei <claudineipereiras@outlook.com>
2021-10-02 14:21:06 +1000

    commit

--- src/components/SliderItem/styles.js
+++ src/components/SliderItem/styles.js
@@ -1,11 +1,19 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.TouchableOpacity`
-  padding: 14px;
+  padding-top: 16px;
+  padding-bottom: 16px;
+  padding-right: 16px;
+  width: 140px;
+  height: 180px;
+
 `;
 
-export const BannerItem = styled.Image``;
-
+export const BannerItem = styled.Image`
+  width: 100%;
+  height: 170px;
+  padding-top: 8px;
+`;
 export const Title = styled.Text``;
 
 export const RateContainer = styled.View``;
