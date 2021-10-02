3ff453f967325871931da9b79383f512a01ed4ee
Claudinei <claudineipereiras@outlook.com>
2021-10-02 13:03:05 +1000

    commit

---
+++ src/components/SliderItem/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function SliderItem(){
+    return(
+        <View>
+            <Text>ITEM</Text>
+        </View>
+    )
+}
+
+export default SliderItem;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,59 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
 
-function home(){
+import { 
+    Container, 
+    SearchContainer,  
+    Input, 
+    SearchButton, 
+    Title, 
+    BannerButton, 
+    Banner,
+    SliderMovie
+} from './styles';
+
+import { Feather } from '@expo/vector-icons';
+
+import Header from '../../components/Header';
+import SliderItem from '../../components/SliderItem';
+
+
+function Home(){
     return(
-        <View>
-            <Text>TELa HOME</Text>
-        </View>
+        <Container>
+            <Header title="React Prime"/>
+
+            <SearchContainer>
+                <Input
+                    placeholder="Ex Vingadores"
+                    placeholderTextColor="#ddd"
+                />
+                <SearchButton>
+                    <Feather name="search" size={30} color="#FFF" />
+                </SearchButton>
+            </SearchContainer>
+
+            <ScrollView>
+                <Title>Em cartaz</Title>
+
+                <BannerButton activeOpacity={0.5} onPress={ ()=> alert('TESTE')}>
+                    <Banner
+                    resizeMethod="resize"
+                    source={{ uri: 'https://images.unsplash.com/photo-1519508234439-4f23643125c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1498&q=80' }}
+                    />
+                </BannerButton>
+
+                <SliderMovie
+                horizontal={true}
+                data={[1,2,3,4]}
+                renderItem={ ({ item }) => <SliderItem/>}
+                />
+                    
+
+            </ScrollView>
+
+        </Container>
     )
 }
+
+export default Home;
