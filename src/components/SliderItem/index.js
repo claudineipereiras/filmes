9bb999586b36edf0d7c8261616cd460b0e75bcc0
Claudinei <claudineipereiras@outlook.com>
2021-10-02 13:56:22 +1000

    commit

--- src/components/SliderItem/index.js
+++ src/components/SliderItem/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
+import {
+    Container,
+    BannerItem,
+    Title,
+    RateContainer,
+    Rate
+ } from './styles'
+
 function SliderItem(){
     return(
-        <View>
+        <Container>
+            <BannerItem
+              source={{ uri: 'https://images.unsplash.com/photo-1519508234439-4f23643125c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1498&q=80'}}
+              />
+            <Title>vingadores</Title>
+            <RateContainer>
+                <Ionicons name="md-star" size={12} color="E7A74e" />
+                <Rate>9/10</Rate>
+
+            </RateContainer>
+
             <Text>ITEM</Text>
-        </View>
+        </Container>
     )
 }
 
---
+++ src/components/SliderItem/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.TouchableOpacity`
+  padding: 14px;
+`;
+
+export const BannerItem = styled.Image``;
+
+export const Title = styled.Text``;
+
+export const RateContainer = styled.View``;
+
+export const Rate = styled.Text``;
