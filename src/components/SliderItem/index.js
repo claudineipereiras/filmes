3ab7859151555f18c2d8a36243c7aade1cef4b7b
Claudinei <claudineipereiras@outlook.com>
2021-10-02 14:20:42 +1000

    commit

--- src/components/SliderItem/index.js
+++ src/components/SliderItem/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-import {
-    Container,
-    BannerItem,
-    Title,
-    RateContainer,
-    Rate
- } from './styles'
+import {Container } from './styles'
+
+import { Ionicons } from '@expo/vector-icons';
 
 function SliderItem(){
     return(
@@ -15,10 +11,12 @@ function SliderItem(){
             <BannerItem
               source={{ uri: 'https://images.unsplash.com/photo-1519508234439-4f23643125c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1498&q=80'}}
               />
-            <Title>vingadores</Title>
+
+            <Title>Vingadores</Title>
+            
             <RateContainer>
-                <Ionicons name="md-star" size={12} color="E7A74e" />
-                <Rate>9/10</Rate>
+              <Ionicons name="md-star" size={12} color="#E7A74e" />
+              <Rate>9/10</Rate>
 
             </RateContainer>
 
